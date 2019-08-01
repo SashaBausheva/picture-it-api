92230209aa73285956e7b5cafa139a9508d5f9c3
SashaBausheva <abausheva@gmail.com>
2019-08-01 00:30:43 -0400

    Change repo name to 'picture-it-api'

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 'use strict'
 
 // creating a base name for the mongodb
-const mongooseBaseName = 'art-reviews-api'
+const mongooseBaseName = 'picture-it-api'
 
 // create the mongodb uri for development and test
 const database = {
